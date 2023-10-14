d6024312b9a6669de671d28d127a662b149f7c54
Stanley <swiftwind0405@gmail.com>
2023-10-14 09:22:18 +0800

    feat: open all

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import {Todos} from './todo/Todos';
 function App() {
   return (
     <div class={styles.App}>
-      {/* <MyComponent name="Stanley"/> */}
-      {/* <Counter/> */}
+      <MyComponent name="Stanley"/>
+      <Counter/>
       <Todos/>
     </div>
   );
